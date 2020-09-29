951af8a940ca69505a244d3dd7e8394904aacb41
Jason Carrillo <jason.carrillo182@gmail.com>
2020-09-29 13:04:40 -0600

    had user enter 3 words then logged them

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -3,15 +3,27 @@ import java.util.Scanner;
 public class ConsoleExercises {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
         double pi = 3.14159;
 
         System.out.printf("The value of pi is approximately %.2f \n", pi);
 
-        Scanner scanner = new Scanner(System.in);
+
 
         int inputNumber = scanner.nextInt();
 
         System.out.println(inputNumber);
+
+        String word1 = scanner.next();
+
+        String word2 = scanner.next();
+
+        String word3 = scanner.next();
+
+        System.out.println(word1);
+        System.out.println(word2);
+        System.out.println(word3);
 
     }
 }
