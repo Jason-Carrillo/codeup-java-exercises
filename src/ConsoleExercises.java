003d6a0272ddfebfc098e20989faf7db973fee23
Jason Carrillo <jason.carrillo182@gmail.com>
2020-09-29 13:06:50 -0600

    user can enter full sentence, and display it bakc

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -5,25 +5,29 @@ public class ConsoleExercises {
 
         Scanner scanner = new Scanner(System.in);
 
-        double pi = 3.14159;
+//        double pi = 3.14159;
+//
+//        System.out.printf("The value of pi is approximately %.2f \n", pi);
+//
+//
+//
+//        int inputNumber = scanner.nextInt();
+//
+//        System.out.println(inputNumber);
+//
+//        String word1 = scanner.next();
+//
+//        String word2 = scanner.next();
+//
+//        String word3 = scanner.next();
+//
+//        System.out.println(word1);
+//        System.out.println(word2);
+//        System.out.println(word3);
 
-        System.out.printf("The value of pi is approximately %.2f \n", pi);
+        String sentence = scanner.nextLine();
 
-
-
-        int inputNumber = scanner.nextInt();
-
-        System.out.println(inputNumber);
-
-        String word1 = scanner.next();
-
-        String word2 = scanner.next();
-
-        String word3 = scanner.next();
-
-        System.out.println(word1);
-        System.out.println(word2);
-        System.out.println(word3);
+        System.out.println(sentence);
 
     }
 }
