99d987d7f8f48423edb421853c7b51a43b27082a
Jason Carrillo <jason.carrillo182@gmail.com>
2020-10-01 12:48:37 -0600

    reformatted loop to use a for loop

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,13 +1,13 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
-        int i = 5;
-
-        int x = 0;
-
-        int y = 100;
-
-        long a = 2;
+//        int i = 5;
+//
+//        int x = 0;
+//
+//        int y = 100;
+//
+//        long a = 2;
 
 //        while (i <= 15){
 //            System.out.println("i = " + i);
@@ -27,13 +27,17 @@ public class ControlFlowExercises {
 //        } while (
 //                y >= -10
 //        );
+//
+//        do {
+//            System.out.println("a = " + a);
+//            a *= a ;
+//        } while (
+//                a <= 1000000
+//        );
 
-        do {
-            System.out.println("a = " + a);
-            a *= a ;
-        } while (
-                a <= 1000000
-        );
+        for(int i = 5; i <= 15; i++){
+            System.out.println("i = " + i);
+        }
 
     }
 }
