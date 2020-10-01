b3064e65c45d5935807491125264c6f8b63540cd
Jason Carrillo <jason.carrillo182@gmail.com>
2020-10-01 12:45:14 -0600

    completed fourth questions

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -3,30 +3,37 @@ public class ControlFlowExercises {
 
         int i = 5;
 
-        while (i <= 15){
-            System.out.println("i = " + i);
-            i++;
-        }
-
         int x = 0;
 
         int y = 100;
 
-        do {
-            System.out.println("x = " + x);
-            x += 2;
-        } while (
-                x <= 100
-        );
+        long a = 2;
+
+//        while (i <= 15){
+//            System.out.println("i = " + i);
+//            i++;
+//        }
+
+//        do {
+//            System.out.println("x = " + x);
+//            x += 2;
+//        } while (
+//                x <= 100
+//        );
+//
+//        do {
+//            System.out.println("y = " + y);
+//            y -= 5;
+//        } while (
+//                y >= -10
+//        );
 
         do {
-            System.out.println("y = " + y);
-            y -= 5;
+            System.out.println("a = " + a);
+            a *= a ;
         } while (
-                y >= -10
+                a <= 1000000
         );
-
-
 
     }
 }
