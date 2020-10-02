33b9fe0a0dc16e36a3c2ffe26896d76e6958bd5f
Jason Carrillo <jason.carrillo182@gmail.com>
2020-10-02 09:34:06 -0600

    third string displays

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -6,7 +6,10 @@ public class StringExercise {
         String check =  "Check " +'"'+"this"+'"'+ " out!, " + '"'+ "s inside of" + '"' + "s!";
         System.out.println(check);
 
+        String wind = "In windows, the main drive is usually C:\\";
+        System.out.println(wind);
 //            In windows, the main drive is usually C:\
+
 //
 //        I can do backslashes \, double backslashes \\,
 //        and the amazing triple backslash \\\!
