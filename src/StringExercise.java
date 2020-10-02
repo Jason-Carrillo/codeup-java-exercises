5f72c40b2a2201044d51a018639f66643f54da98
Jason Carrillo <jason.carrillo182@gmail.com>
2020-10-02 09:29:43 -0600

    got second string to display

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,2 +1,18 @@
 public class StringExercise {
+    public static void main(String[] args) {
+        String edu = "We don't need no education\n";
+
+        String cont = "We don't need no thought control";
+        System.out.println(edu + cont );
+
+        String check =  "Check " +'"'+"this"+'"'+ " out!, " + '"'+ "s inside of" + '"' + "s!";
+
+        System.out.println(check);
+//            In windows, the main drive is usually C:\
+//
+//        I can do backslashes \, double backslashes \\,
+//        and the amazing triple backslash \\\!
+
+
+    }
 }
