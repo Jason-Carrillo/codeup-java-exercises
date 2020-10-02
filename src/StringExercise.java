61edc160a6beae85eadc3603ce78774714621358
Jason Carrillo <jason.carrillo182@gmail.com>
2020-10-02 09:30:39 -0600

    reformatted first string

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,13 +1,11 @@
 public class StringExercise {
     public static void main(String[] args) {
-        String edu = "We don't need no education\n";
-
-        String cont = "We don't need no thought control";
-        System.out.println(edu + cont );
+        String edu = "We don't need no education\nWe don't need no thought control";
+        System.out.println(edu);
 
         String check =  "Check " +'"'+"this"+'"'+ " out!, " + '"'+ "s inside of" + '"' + "s!";
-
         System.out.println(check);
+
 //            In windows, the main drive is usually C:\
 //
 //        I can do backslashes \, double backslashes \\,
