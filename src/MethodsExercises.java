3420e1cfebb98e936c9280ad069654ae95413408
Jason Carrillo <jason.carrillo182@gmail.com>
2020-10-02 15:03:10 -0600

    completed getInteger method

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,13 +1,19 @@
+import java.util.Scanner;
+
 public class MethodsExercises {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Addition(1, 2) = " + Addition(1, 2));
         System.out.println("Subtraction(2, 3) = " + Subtraction(2, 3));
         System.out.println("Multiplication(4, 5) = " + Multiplication(4, 5));
         System.out.println("Division(5, 5) = " + Division(5, 5));
         System.out.println("Modulus(1, 2) = " + Modulus(1, 2));
+        System.out.println("getInteger(1, 20) = " + getInteger(1, 20));
 
+
+// END of main
     }
 
     public static int Addition(int num, int num2){
@@ -28,4 +34,11 @@ public class MethodsExercises {
         return num % num2;
     }
 
+    public static String getInteger(int min, int max){
+        return "Enter a number between " + min + " and " + max;
+    }
+
+
+
+    //END of Class
 }
