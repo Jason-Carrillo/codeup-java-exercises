8c8864d4e1ca684d7c06ca2874887a59423d8c09
Jason Carrillo <jason.carrillo182@gmail.com>
2020-10-02 10:49:22 -0600

    completed bob question

--- src/Bob.java
+++ src/Bob.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Bob {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Tell Bob Something...");
+
+        String userInput = scanner.nextLine();
+
+        if (userInput.equals("")){
+            System.out.println("Bob: Fine. Be that way!");
+        } else if (userInput.charAt(userInput.length()-1) == '!'){
+            System.out.println("Bob: Whoa, chill out!");
+        } else if (userInput.charAt(userInput.length()-1) == '?'){
+        System.out.println(userInput.charAt(userInput.length()-1));
+        System.out.println("Bob: Sure.");
+        } else {
+            System.out.println("Bob: Whatever.");
+        }
+
+
+        // end of Main
     }
 }
