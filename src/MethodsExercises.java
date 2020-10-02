4093593ce528d9102adc25d0b0155122771eb465
Jason Carrillo <jason.carrillo182@gmail.com>
2020-10-02 15:43:11 -0600

    completed factorial question

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -3,16 +3,13 @@ import java.util.Scanner;
 public class MethodsExercises {
     public static void main(String[] args) {
 
-        System.out.println("Addition(1, 2) = " + Addition(1, 2));
-        System.out.println("Subtraction(2, 3) = " + Subtraction(2, 3));
-        System.out.println("Multiplication(4, 5) = " + Multiplication(4, 5));
-        System.out.println("Division(5, 5) = " + Division(5, 5));
-        System.out.println("Modulus(1, 2) = " + Modulus(1, 2));
-        System.out.println("getInteger(1, 20) = " + getInteger(1, 20));
-
-
-
-
+//        System.out.println("Addition(1, 2) = " + Addition(1, 2));
+//        System.out.println("Subtraction(2, 3) = " + Subtraction(2, 3));
+//        System.out.println("Multiplication(4, 5) = " + Multiplication(4, 5));
+//        System.out.println("Division(5, 5) = " + Division(5, 5));
+//        System.out.println("Modulus(1, 2) = " + Modulus(1, 2));
+//        System.out.println("getInteger(1, 20) = " + getInteger(1, 20));
+        System.out.println("factorial() = " + factorial());
 
 
 // END of main
@@ -49,6 +46,25 @@ public class MethodsExercises {
         }
         return userInt;
     }
+
+    public static long factorial(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter a number between 1, and 10");
+        int num = scanner.nextInt();
+        if (num > 10 || num < 1){
+
+            return factorial();
+        } else {
+            long result = 1;
+            for(int i = 1; i <= num; i++){
+                result *= i;
+            }
+            System.out.println(result);
+            return result;
+        }
+    }
+
+
 
 
 
