64a3dd32b77b142ca5c564c902e0b5ef6864dcdc
Jason Carrillo <jason.carrillo182@gmail.com>
2020-10-05 10:12:23 -0600

    completed exercise

---
+++ src/HighLow.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class HighLow {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int range = 100 - 1 + 1;
+        int numbertoGuess = (int) ((Math.random() * range) + 1);
+
+        while(true){
+            System.out.println("Guess a number between 1 and 100");
+            short userInput = Short.parseShort(scan.nextLine());
+
+            if (userInput > numbertoGuess){
+                System.out.println("LOWER");
+            } else if (userInput < numbertoGuess) {
+                System.out.println("HIGHER");
+            } else {
+                System.out.println("GOOD GUESS!");
+                break;
+            }
+
+        }
+    }
+}
