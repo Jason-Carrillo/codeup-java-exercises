196a808f917ed95394636483cb180adf1f606605
Jason Carrillo <jason.carrillo182@gmail.com>
2020-10-05 13:07:21 -0600

    added dish from class

--- src/Dish.java
+++ src/Dish.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Dish {
+public class Dish {
+    public static void main(String[] args) {
+        ;
+
+    }
+
+    public int costInCents;
+
+    public String nameOfDish;
+
+    public Boolean wouldRecommend;
+
+    public void printSummary(){
+        System.out.println("Cost: " + costInCents + "\nName: " + nameOfDish + "\nRecommended: " + wouldRecommend);
+
+
+    }
 }
--- src/DishTest.java
+++ src/DishTest.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class DishTest {
+public class DishTest {
+    public static void main(String[] args) {
+
+
+        Dish dish1 = new Dish();
+
+    }
 }
