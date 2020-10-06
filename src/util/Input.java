ce99741eb0dcdd1162059097c61562f838aa77f1
Jason Carrillo <jason.carrillo182@gmail.com>
2020-10-06 10:57:13 -0600

    added getInt method

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,4 +1,37 @@
 package util;
 
+import java.util.Scanner;
+
 public class Input {
+    private Scanner scanner;
+
+    public String getString(){
+        return this.scanner.nextLine();
+    }
+
+    public int getInt(){
+        return this.scanner.nextInt();
+    }
+
+    public boolean yesNo(){
+        System.out.println("Would you like to conitnue? Yes/No");
+
+        String input = this.getString();
+
+        return input.equalsIgnoreCase("yes");
+    }
+
+    int getInt(int min, int max){
+        System.out.printf("Enter a number between %d, and %d", min, max);
+
+        int input = this.getInt();
+
+        if (input < min || input > max){
+            getInt(min, max);
+        }
+        return input;
+    }
+
+
+//    END of class
 }
