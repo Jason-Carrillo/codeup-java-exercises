c3e8a43a69f1adbf7a29f9d7e6b20a980550d794
Jason Carrillo <jason.carrillo182@gmail.com>
2020-10-06 13:35:10 -0600

    cleaned up yesno on input class

--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -3,13 +3,13 @@ package util;
 public class InputTest {
     public static void main(String[] args) {
 
-
-
         Input test = new Input();
 
-        int userInt = test.getInt();
-
-        userInt = test.getInt(5, 10);
+        if(test.yesNo()){
+            System.out.println("This returned True");
+        } else {
+            System.out.println("This returned False");
+        }
 
     }
 }
