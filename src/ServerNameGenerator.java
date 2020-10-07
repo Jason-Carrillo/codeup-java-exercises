edca3ae0252015ba1d616268f2c931ffcb043c82
Jason Carrillo <jason.carrillo182@gmail.com>
2020-10-07 10:19:20 -0600

    added list of 10 adjectives, and list of 10 nouns

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,5 +1,11 @@
 public class ServerNameGenerator {
     public static void main(String[] args) {
 
+        String[] adjectives = {"nice", "beautiful", "smelly", "plain", "scary", "clean", "fit", "happy", "jollu", "kind"};
+
+        String[] nouns = {"Texas", "Arizona", "Mall", "Jail", "Belt", "Line", "Boat", "Fish", "Planet", "Plants"};
+
+
+
     }
 }
