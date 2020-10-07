185b8a70e419d0a08ab7ba72bf30a1ef7166150c
Jason Carrillo <jason.carrillo182@gmail.com>
2020-10-07 10:34:00 -0600

    created method that grabs 2 random words from the adjectives, and noun list, and presents them together

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,11 +1,22 @@
 public class ServerNameGenerator {
     public static void main(String[] args) {
 
-        String[] adjectives = {"nice", "beautiful", "smelly", "plain", "scary", "clean", "fit", "happy", "jollu", "kind"};
+        String[] adjectives = {"nice", "beautiful", "smelly", "plain", "scary", "clean", "fit", "happy", "jolly", "kind"};
 
         String[] nouns = {"Texas", "Arizona", "Mall", "Jail", "Belt", "Line", "Boat", "Fish", "Planet", "Plants"};
 
+//        System.out.println(Math.round(Math.random() * 10));
+        System.out.println(randomizer(adjectives, nouns));
 
-
+//        END of main
     }
+
+
+    public static String randomizer(String [] adjectives, String[] nouns){
+        String adj = adjectives[(int) Math.round(Math.random() * 10)];
+        String noun = nouns[(int) Math.round(Math.random() * 10)];
+
+        return adj + " " + noun;
+    }
+
 }
