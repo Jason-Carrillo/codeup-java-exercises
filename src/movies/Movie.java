e078fea2e221fd3174b35f7d28ccca5de34ec080
Jason Carrillo <jason.carrillo182@gmail.com>
2020-10-07 10:59:35 -0600

    added contructor method, and get info method

--- src/movies/Movie.java
+++ src/movies/Movie.java
@@ -1,2 +1,14 @@
-package movies;public class Movie {
+package movies;
+public class Movie {
+    private String name;
+    private String category;
+
+    public Movie(String movieName, String movieCategory) {
+        name = movieName;
+        category = movieName;
+    }
+
+    public String getMovie() {
+        return String.format("Name: %s, Category: %s", name, category);
+    }
 }
