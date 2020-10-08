7cf82c2f29d70cc80701b37759dc6b3303dc05a6
Jason Carrillo <jason.carrillo182@gmail.com>
2020-10-08 14:42:26 -0600

    added area, and perimeter methods

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -4,4 +4,18 @@ public class Rectangle {
     protected String length;
     protected String width;
 
+
+    public Rectangle(String length, String width){
+        this.length = length;
+        this.width = width;
+    }
+
+    public int getArea(int length, int width){
+        return 2 * length + 2 * width;
+    }
+
+    public int getPerimeter(int length, int width){
+        return length * width;
+    }
+
 }
