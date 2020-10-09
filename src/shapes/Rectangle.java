67d9efdd8e766264dbf4bd29fa693aa0ef79c727
Jason Carrillo <jason.carrillo182@gmail.com>
2020-10-09 08:44:36 -0600

    cleaned up code

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -5,20 +5,17 @@ public class Rectangle {
     protected int width;
 
 
-    public Rectangle(int length, int width){
-        this.length = length;
-        this.width = width;
+    public Rectangle(int userWidth, int userLength){
+        this.length = userLength;
+        this.width = userWidth;
     }
 
-    public int getArea(int length, int width){
-        this.length = length;
-        this.width = width;
-
-        return 2 * this.length + 2 * this.width;
+    public int getPerimeter(){
+        return (2 * this.length) + (2 * this.width);
     }
 
-    public int getPerimeter(int length, int width){
-        return length * width;
+    public int getArea(){
+        return this.length * this.width;
     }
 
 }
--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -5,11 +5,11 @@ public class ShapesTest {
         Rectangle box1 = new Rectangle(5, 4);
         Rectangle box2 = new Square(5);
 
-        System.out.println(box1.getArea(box1.length, box1.width));
-        System.out.println(box1.getPerimeter(box1.length, box1.width));
+        System.out.println(box1.getArea());
+        System.out.println(box1.getPerimeter());
 
-        System.out.println(box2.getArea(box2.length, box2.width));
-        System.out.println(box2.getPerimeter(box2.length, box2.width));
+        System.out.println(box2.getArea());
+        System.out.println(box2.getPerimeter());
 
     }
 
