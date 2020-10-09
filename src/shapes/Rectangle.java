258c3c165d87b50b2517a69b1b70c09089efab90
Jason Carrillo <jason.carrillo182@gmail.com>
2020-10-09 11:11:21 -0600

    removed code from recntangle and square classes

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,21 +1,5 @@
 package shapes;
 
 public class Rectangle {
-    protected int length;
-    protected int width;
-
-
-    public Rectangle(int userWidth, int userLength){
-        this.length = userLength;
-        this.width = userWidth;
-    }
-
-    public int getPerimeter(){
-        return (2 * this.length) + (2 * this.width);
-    }
-
-    public int getArea(){
-        return this.length * this.width;
-    }
 
 }
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -2,8 +2,4 @@ package shapes;
 
 public class Square extends Rectangle {
 
-        public Square(int side){
-            super(side, side);
-        }
-
 }
