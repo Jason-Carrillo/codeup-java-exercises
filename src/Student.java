35a4eb75041d52a0ddaaf707c4b5a90a09728626
Jason Carrillo <jason.carrillo182@gmail.com>
2020-10-13 10:57:33 -0600

    added Student class and private properties

---
+++ src/Student.java
@@ -0,0 +1,10 @@
+public class Student {
+    private String name;
+    private int[] grade;
+
+    public Student(String studentName, int[] studentGrade){
+        this.name = studentName;
+        this.grade = studentGrade;
+    }
+
+}
