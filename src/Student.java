341765e468d6fdc6d38cf81bac37f94b669eeb99
Jason Carrillo <jason.carrillo182@gmail.com>
2020-10-13 11:12:58 -0600

    added all methods

--- src/Student.java
+++ src/Student.java
@@ -1,10 +1,28 @@
+import java.util.ArrayList;
+
 public class Student {
     private String name;
-    private int[] grade;
+    private ArrayList<Integer> grade = new ArrayList<>();
 
-    public Student(String studentName, int[] studentGrade){
+    public Student(String studentName, int studentGrade){
         this.name = studentName;
-        this.grade = studentGrade;
+        grade.add(studentGrade);
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void addGrade(int grade){
+        this.grade.add(grade);
+    }
+
+    public double getGradeAverage(){
+        double totalGrade = 0;
+        for (Integer number: grade){
+             totalGrade += number;
+        }
+        return totalGrade/grade.size();
     }
 
 }
